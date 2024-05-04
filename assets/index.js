cf074829bf232ac9dfc9a3ba63278110be7cc95e
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-05-04 22:19:21 +0100

    added to CSS and JS.

--- assets/index.js
+++ assets/index.js
@@ -36,8 +36,6 @@ publishBtnEl.addEventListener("click", function () {
   push(championsInDB, [endorsementValue, fromValue, toValue]);
 
   clearInput();
-
-  addComment();
 });
 
 onValue(championsInDB, function (snapshot) {
@@ -45,23 +43,57 @@ onValue(championsInDB, function (snapshot) {
     // will only fetch items from the database if there is any
     let commentsArray = Object.entries(snapshot.val()); //converts snapshot.val() from an object to an Array. Entries gives both the id and value in the array.
 
+    clearEndorsementEl();
+
     for (var i = 0; i < commentsArray.length; i++) {
       let comments = commentsArray[i];
-      let commentsID = comments[0];
-      let commentsValue = comments[1];
 
       addComment(comments); //appends each item to the comment element for each iteration.
     }
+  } else {
+    endorsementEl.innerHTML = "No feedback yet.";
   }
 });
 
 function addComment(comment) {
-  let commentID = comment[0];
-  let commentValue = comment[1];
+  let commentId = comment[0];
+  let commentData = comment[1];
+  let commentInput = commentData[0];
+  let commentFrom = commentData[1];
+  let commentTo = commentData[2];
+
+  let newEl = $("<li>");
+  let contentEl = $("<div>");
+  let toInputEl = $("<h3>");
+  let fromInputEl = $("<h3>");
+  let endorsementInputEl = $("<p>");
+  let likeFromContainer = $("<div>");
+
+  toInputEl.textContent = `To ${commentTo}`;
+  endorsementInputEl.textContent = commentInput;
+  fromInputEl.textContent = `From ${commentFrom}`;
+
+  newEl.appendChild(contentEl);
+  contentEl.appendChild(toInputEl);
+  contentEl.appendChild(endorsementInputEl);
+  contentEl.appendChild(likeFromContainer);
+  likeFromContainer.appendChild(fromInputEl);
+
+  contentEl.addClass("content");
+  toInputEl.addClass("to-text");
+  fromInputEl.addClass("from-text");
+  endorsementInputEl.addClass("endorsement-text");
+  likeFromContainer.addClass("like-from-container");
+
+  endorsementEl.append(newEl);
 }
 
 function clearInput() {
   endorsementInputEl.value = "";
   fromInputEl.value = "";
   toInputEl.value = "";
+}
+
+function clearEndorsementEl() {
+  endorsementEl.innerHTML = "";
 }
